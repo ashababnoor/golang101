0a2f9cb8ed0cf67407611ac3ece3e2c7d008265a
Shabab Noor <ashababnoor@gmail.com>
2023-10-01 00:38:26 +0600

    broke code to check if test code is working

--- tutorials/02-fundamentals/greetings/greetings.go
+++ tutorials/02-fundamentals/greetings/greetings.go
@@ -13,7 +13,8 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 	// Create a message using a random format.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprintf(randomFormat())
 	return message, nil
 }
 
